a00a9bdadb82d13fa48896e06f90629aa367efc6
vetri02 <vetri02@gmail.com>
2011-10-17 00:28:16 +0530

    accounting.js for formatting result

--- js/main.js
+++ js/main.js
@@ -22,7 +22,6 @@ var cc = {
 		    $.getJSON(
 		        'http://openexchangerates.org/latest.json',
 		        function(data) {
-					//console.log(data)
 					cc.jsonData = data;
 		            // Check money.js has finished loading:
 		            if ( typeof fx !== "undefined" && fx.rates ) {
@@ -35,60 +34,21 @@ var cc = {
 		                    base : data.base
 		                }
 		            }
-					//console.log(this.jsonData)
-					//cc.buildSelectBox();
 		        	cc.buildOptionBox();
 				}
 		    );
 		
-	},
-	buildSelectBox:function(){
-		   this.curValue = this.jsonData.rates
-			console.log(this.curValue);
-		   for(var key in this.curValue){
-		      this.keys.push(key);	
-		   }
-		   for(var i=0;i<=this.keys.length;i++){
-			 var objOption = document.createElement("option");
-			  objOption.text = this.keys[i];
-			  objOption.value = this.keys[i];
-			  document.getElementById("fromSelect").add(objOption);
-		   }
-		   for(var i=0;i<=this.keys.length;i++){
-			 var objOption = document.createElement("option");
-			  objOption.text = this.keys[i];
-			  objOption.value = this.keys[i];
-			  document.getElementById("toSelect").add(objOption);
-		   }
-			this.fromValue = document.getElementById("fromSelect").value;
-			this.toValue = document.getElementById("toSelect").value;
-			
-			$("#fromSelect").change(
-				function(){cc.fromValue = this.value; cc.from();}
-			);
-			$("#toSelect").change(
-				function(){cc.toValue = this.value; cc.to();}
-			);
-			$("#go").click(function(){
-				cc.showRes();
-				}	
-			);
-			//this.showRes();
-			
 	},
 	buildOptionBox:function(){
 			$.getJSON(
 		        'js/currencies.json',
 		        function(data) {
 					this.curValue = jQuery.parseJSON(data);
-						console.log(this.curValue);
 					   for(var key in data){
 					      cc.keys.push(key);
-							console.log(cc.keys);
 					   }
 						for(var key in data){
 						      cc.values.push(data[key]);
-								console.log(data[key]);
 				   	   }
 					for(var i=0;i<cc.keys.length;i++){
 						 var objOption = document.createElement("option");
@@ -102,37 +62,37 @@ var cc = {
 						  objOption.value = cc.keys[i];
 						  document.getElementById("toSelect").add(objOption);
 					   }
-						//$("#fromSelect").chosen();
-						//$("#toSelect").chosen();
 						cc.fromValue = document.getElementById("fromSelect").value;
 						cc.toValue = document.getElementById("toSelect").value;
 
 						$("#fromSelect").chosen().change(
-							function(){cc.fromValue = this.value; cc.from();}
+							function(){cc.fromValue = this.value;}
 						);
 						$("#toSelect").chosen().change(
-							function(){cc.toValue = this.value; cc.to();}
+							function(){cc.toValue = this.value;}
 						);
 						$("#go").click(function(){
 							cc.showRes();
 							}	
 						);
-						//this.showRes();
 				}
 			);
 	},
-	from:function(){
-		console.log(this.fromValue)
-		this.showRes();
-	},
-	to:function(){
-		console.log(this.toValue)
-		this.showRes();
-	},
 	showRes:function(){
 		fx.settings = { from: this.fromValue, to: this.toValue };
-		console.log(fx.convert($("#amount").val()));
-		$("#result").text(fx.convert($("#amount").val()))
+		//$("#toCur").text(this.toValue);
+		this.money = fx.convert($("#amount").val());
+		//console.log(accounting.formatMoney(cc.money, { symbol: cc.toValue,  format: "%v %s" }));
+		$("#result").text(accounting.formatMoney(cc.money, { 
+			symbol: cc.toValue,  
+			format: {
+				pos : "%v %s ",
+				neg : "(%v) %s",
+				zero: "-- %s"
+			} 
+		}));
+		//fx.settings = { from: this.fromValue, to: this.toValue };
+		//$("#result").text(fx.convert($("#amount").val()))
 	}
 	
 };
